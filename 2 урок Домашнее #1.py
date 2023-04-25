a56b3097e9832d4f4a5ea72380b0434585f6d06e
ainom <ainomakoto@gmail.com>
2023-04-25 19:50:09 +0200

    Урок 2 Домшнее Задание

--- 2 урок Домашнее #1.py
+++ 2 урок Домашнее #1.py
@@ -1,11 +1,8 @@
-x = int(input('Input 4 digit number: ' ))
+x = int(input('Input 4 digit number: '))
 
 n1 = x //1000
-
 n2 = x %10
-
 n3 = (x%100)//10
-
 n4 = (x//100)%10
 
 print(n1)
---
+++ 2 урок Домашнее #2.py
@@ -0,0 +1,12 @@
+x = int(input('Input 6 digit number: '))
+y = x//1000
+z = x % 1000
+
+n1 = y // 100
+n2 = y // 10 % 10
+n3 = y % 10
+n4 = z // 100
+n5 = z // 10 % 10
+n6 = z % 10
+
+print(n6,n5,n4,n3,n2,n1)
